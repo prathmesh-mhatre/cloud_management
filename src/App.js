6b61d43e5d4c0db31972263481196e7f9f74e373
Prathmesh Mhatre <prathmesh.mhatre99@gmail.com>
2021-12-26 02:17:46 +0530

    progress-bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Onboarding from "./Components/Onboarding";
+import Home from "./Components/Home";
 function App() {
   return (
     <>
-      <Onboarding />
+      <Home />
     </>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,46 @@
 import React from "react";
+import onboarding_icon from "../assets/onboarding_icon.png";
+
+let i = "35%";
 
 function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className="home-container">
+        <div className="sub-container">
+          <section className="greeting-section">
+            <div className="greeting">Hello Jessie, </div>
+            <div className="sub-text">at the moment you have</div>
+            <div className="free-storage">
+              <div className="storage-amount">32,5 GB</div>
+              <div className="sub-text2">of 100 GB free</div>
+              <img
+                src={onboarding_icon}
+                className="up-arrow"
+                alt=" upword arrow icon"
+              />
+            </div>
+            <div className="storage-indicator">
+              <div className="pink" style={{ width: i }}></div>
+            </div>
+          </section>
+        </div>
+        <section className="files-folders">
+          <div className="sub-container">
+            <input
+              className="search-bar"
+              type="text"
+              defaultValue="      Search"
+            />
+          </div>
+          <section className="tabs">
+            <div className="tab">Files</div>
+            <div className="tab">FOLDERS</div>
+          </section>
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default Home;
