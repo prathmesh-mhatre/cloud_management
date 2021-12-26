357b1d17b6d3c30afb7f10832406fd3e41e5defa
prathmesh-mhatre <prathmesh.mhatre99@gmail.com>
2021-12-26 06:09:07 +0530

    dummy data for home page

---
+++ src/Data/homeData.js
@@ -0,0 +1,134 @@
+const homeData = {
+  userName: "Jessie",
+  freeData: 35.2,
+  folders: [
+    {
+      folderName: "The next big thing",
+      subFolders: 7,
+      floderSize: "2.1 gb",
+      users: [
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+      ],
+    },
+    {
+      folderName: "Top Secret",
+      subFolders: 7,
+      floderSize: "523 mb",
+      users: [
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+      ],
+    },
+    {
+      folderName: "Freebie Project",
+      subFolders: 3,
+      floderSize: "192 mb",
+      users: [
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+      ],
+    },
+    {
+      folderName: "London Meetup",
+      subFolders: 453,
+      floderSize: "1.7 gb",
+      users: [
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+        {
+          profilePicture:
+            "https://pbs.twimg.com/profile_images/1433409026075676673/8f0B9htV_400x400.jpg",
+        },
+      ],
+    },
+  ],
+};
